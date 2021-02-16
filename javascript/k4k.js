0b091f59f6d4dd956e4247918ff4771eccc1b8dd
Janet Raynor <raynor.janet@gmail.com>
2021-02-15 18:00:59 -0800

    v1

--- javascript/k4k.js
+++ javascript/k4k.js
@@ -1,10 +1,9 @@
-const DEFAULT_BUTTON = 'appointments';
 const availableButtons = ['appointments', 'how-it-works', /*'volunteering'*/]
+const DEFAULT_BUTTON = 'appointments';
+const CONTACT_EMAIL = 'k4kvolunteering@gmail.com';
 
 const init = () => {
-    Array.from(document.querySelectorAll('#menu li a')).forEach(button => button.style.display = 'none');
-
-    availableButtons.forEach(name => {
+    Array.from(document.querySelectorAll('#menu li a')).forEach(button => button.style.display = 'none'); availableButtons.forEach(name => {
         const button = document.getElementById(`${name}-button`);
         if (button) {
             button.style.display = 'block';
@@ -14,10 +13,14 @@ const init = () => {
             console.log(`button not found for ${name}`)
         }
     })
+    Array.from(document.querySelectorAll('#appt-instructions li')).forEach((li, i) => {
+        li.classList += (i % 2 == 1) ? ' odd' : ' even'
+    });
+    Array.from(document.querySelectorAll('a.email')).forEach(mailLink => mailLink.setAttribute('href', `mailto:${CONTACT_EMAIL}`));
+
     const qsParams = getParams(window.location.search);
     const defaultButton = qsParams['button'] || DEFAULT_BUTTON;
     navAction(defaultButton)
-    //initVimeo()
 }
 
 const navAction = (which) => {
@@ -29,66 +32,19 @@ const navAction = (which) => {
 }
 
 const getParams = (searchString) => {
-    const params = [];
+    const params = {};
     try {
+        const paramArray = [];
         const pairStrs = searchString.replace('?', '').split('&');
         // special setup for single parameter to indicate default button
-        if (pairStrs.length == 1 && !pairStrs.includes('=')) {
-            return [{ button: pairsStr }]
-        }
         pairStrs.forEach(pair => {
-            const pName = pair.split.length == 2 ? pair.split('=')[0] : 'button';
-            const pVal = pair.split.legnth == 2 ? pair.split('=')[1] : pair.split('=')[0];
-            params.push({ [pName]: pVal });
+            const pName = pair.split('=').length == 2 ? pair.split('=')[0] : 'button';
+            const pVal = pair.split('=').legnth == 2 ? pair.split('=')[1] : pair.split('=')[0];
+            paramArray.push({ [pName]: pVal });
         })
+        paramArray.forEach(param => Object.assign(params, param))
     } catch (err) {
-        return [];
+        return {};
     }
     return params;
 }
-
-
-const initVimeo = () => {
-    const placeholder = document.querySelector('.vp-placeholder');
-    const placeholderThumb = document.querySelector('.vp-placeholder-thumb');
-    const videoWidth = parseInt('960');
-    const videoHeight = parseInt('540');
-    const thumb = new Image();
-    thumb.onload = function () {
-        const videoAspectRatio = videoWidth / videoHeight;
-        const imageAspectRatio = this.width / this.height;
-        if (imageAspectRatio <= 0.95 * videoAspectRatio || imageAspectRatio >= 1.05 * videoAspectRatio) {
-            const rect = placeholder.getBoundingClientRect();
-            const placeholderWidth = rect.right - rect.left;
-            const placeholderHeight = rect.bottom - rect.top;
-            const viewportWidth = window.innerWidth / placeholderWidth * 100;
-            const viewportHeight = window.innerHeight / placeholderHeight * 100;
-            placeholderThumb.style.height = 'calc(' + this.height + ' / ' + this.width + ' * ' + viewportWidth + 'vw)';
-            placeholderThumb.style.maxWidth = 'calc(' + this.width + ' / ' + this.height + ' * ' + viewportHeight + 'vh)';
-        }
-        placeholder.style.visibility = 'visible';
-    };
-    thumb.src = "https://i.vimeocdn.com/video/722705085.jpg?mw=80&q=85";
-}
-
-
-const variableSafe = (anyString) => {
-    if (!anyString === '') {
-        return 'empty';
-    }
-    let retVal = anyString;
-    if (isInteger('' + anyString.charAt(0))) {
-        retVal = digits[parseInt(charAt(0))] + '-' + anyString.slice(1);
-    }
-    retVal = retVal.trim()
-        .toLowerCase()
-        .replace(/\s+/g, '-')
-        .replace(/_+/, '-')
-        .replace(/\W+/g, '')
-        .replace(/w+/g, '-')
-        .replace(/-+/, '-');
-    console.log(`${anyString} converted to ${retVal}`);
-    return retVal;
-}
-
-const digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']

f14096759715c48944b2af29d5fc9f09b4b0f032
Janet Raynor <raynor.janet@gmail.com>
2022-02-19 16:48:54 -0800

    wip

--- javascript/k4k.js
+++ javascript/k4k.js
@@ -1,4 +1,4 @@
-const availableButtons = ['appointments', 'how-it-works', 'contact-us']
+const availableButtons = ['appointments', 'how-it-works', 'volunteering']
 const DEFAULT_BUTTON = 'appointments';
 const CONTACT_EMAIL = 'k4kvolunteering@gmail.com';
 
